f812b178feb47d0cb478af22a99a2afd015bb66b
CHILLSON\naxx1000 <naxx1000@gmail.com>
2022-12-03 14:26:39 +0100

    Use day2 folder for RockPaperScissors assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 import day1.CalorieCounting
-import day1.RockPaperScissors
+import day2.RockPaperScissors
 
 fun main(args: Array<String>) {
 
--- src/main/kotlin/day2/RockPaperScissors.kt
+++ src/main/kotlin/day2/RockPaperScissors.kt
@@ -1,4 +1,4 @@
-package day1
+package day2
 
 import LoseDrawWin
 import RPS
